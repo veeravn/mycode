e070003cf52e59e1b1067208ab2338fb3602b934
Veerav Naidu <veeravn@gmail.com>
2021-11-29 17:47:09 +0000

    learning about go basics

---
+++ ready-to-learn-about.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+    "math/rand"
+)
+
+func main() {
+    fmt.Println("Looks like you're ready to learn GoLang with Alta3 Research")
+    fmt.Println("My favorite number is 42")
+    fmt.Println("However, I grab random numbers as well", rand.Intn(10))
+}
+
+
