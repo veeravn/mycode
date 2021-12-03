2233f92db8d3fb16421a7f04513b7a6f9f26f1d7
Veerav Naidu <veeravn@gmail.com>
2021-12-03 16:15:54 +0000

    sql with go

---
+++ mysql/mysql03.go
@@ -0,0 +1,40 @@
+/* Alta3 Research | RZFeeser
+   mySQL - Insert a row into a table within our database */
+   
+package main
+
+import (
+    "database/sql"
+    "fmt"
+    "log"
+
+    _ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+
+    db, err := sql.Open("mysql", "student:playstation5@tcp(127.0.0.1:3306)/testdb")
+    defer db.Close()
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // this is the data we wish to insert into our program
+    sql := "INSERT INTO cities(name, population) VALUES ('Moscow', 12506000)"
+    res, err := db.Exec(sql)
+
+    if err != nil {
+        panic(err.Error())
+    }
+
+    // With LastInsertId, we get the last inserted id
+    lastId, err := res.LastInsertId()
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Printf("The last inserted row id: %d\n", lastId)
+}
+
